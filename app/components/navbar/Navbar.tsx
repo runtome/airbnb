18e3af7ffc02a94788cbb51851e32691242c7cab
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-13 15:31:13 +0700

    Add Search nav

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,25 +1,30 @@
 import Container from "@/app/components/Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
     return(
         <div className="fixed w-full bg-white z-10 shadow-sm">
             <div
                 className="
-                py-4
-                border-b-[1px]">
+                py-4 
+                border-b-[1px]
+                ">
                     <Container>
                        <div
                         className="
-                         flex
-                         flex-row
-                         item-center
-                         justify-between
-                         gap-3
-                         md:gap-0
-                         "
+                        flex 
+                        flex-row 
+                        items-center 
+                        justify-between
+                        gap-3
+                        md:gap-0
+                        "
                         >
                             <Logo />
+                            <Search/>
+
+
                        </div>
                     </Container>
             </div>
