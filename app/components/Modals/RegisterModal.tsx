397bdf684fbd961d4e15a796eeddf84c6616b5c5
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-14 21:59:07 +0700

    Add github and google login

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -15,6 +15,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import toast from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModel = () => {
     const registermModal = useRegisterModal();
@@ -83,6 +84,49 @@ const RegisterModel = () => {
         </div>
     )
 
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button
+             outline
+             label="Continue with Google"
+             icon={FcGoogle}
+             onClick={() => {}}
+            />
+            <Button
+             outline
+             label="Continue with Github"
+             icon={AiFillGithub}
+             onClick={() => {}}
+            />
+            <div
+             className="
+              text-neutral-500
+              text-center
+              mt-4
+              font-light
+             "
+            >
+                <div className="justify-center flex flex-row items-center gap-2">
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div
+                     onClick={registermModal.onClose}
+                     className="
+                      text-neutral-800
+                      cursor-pointer
+                      hover:underline
+                     "
+                    >
+                        Log in
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+
 
     return(
         <Modal 
@@ -93,6 +137,7 @@ const RegisterModel = () => {
          onClose={registermModal.onClose}
          onSubmit={handleSubmit(onSubmit)}
          body={bodyContent}
+         footer={footerContent}
         />
 
     )
