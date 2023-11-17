c761397957f9c403434084243d5e59bf92c2b33f
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-17 22:47:05 +0700

    Make ListingCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListing";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0){
     return(
@@ -32,7 +35,11 @@ export default async function Home() {
         >
           {listings.map((listing:any)=> {
             return(
-              <div>{listing.title}</div>
+              <ListingCard 
+              currentUser = {currentUser}
+              key={listing.id}
+              data={listing}              
+              />
             )
           })}
         </div>
