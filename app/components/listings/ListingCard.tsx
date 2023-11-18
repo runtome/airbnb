be7b97e0ee9f2b4cd04f2b9600acd29cf7193013
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-18 21:22:31 +0700

    Individual Listing View : 1 Prepare end point

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -66,7 +66,7 @@ const ListingCard: React.FC<ListingCardProps>= ({
 
     return(
         <div 
-      onClick={() => router.push(`/listings/${data.id}`)} 
+      onClick={() => router.push(`/listings/${data.id}`)}  // End point that has been prepare 
       className="col-span-1 cursor-pointer group"
     >
       <div className="flex flex-col gap-2 w-full">
---
+++ app/listings/[listingId]/page.tsx
@@ -0,0 +1,10 @@
+const ListingPage = () => {
+    return(
+        <div>
+            My Listing page 
+
+        </div>
+    );
+}
+
+export default ListingPage;
