619c39f57f8cb34fd775fe7a741f482325f1eb83
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-18 21:05:24 +0700

    Communicate string between severside and client

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Listing, Reservation } from "@prisma/client";
 
-import { SafeUser } from "@/app/types";
+import { SafeListings, SafeUser } from "@/app/types";
 import { useRouter } from "next/navigation";
 import useCountries from "@/app/hooks/useCountries";
 import { useCallback, useMemo } from "react";
@@ -11,7 +11,7 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-    data: Listing;
+    data: SafeListings;
     reservation?: Reservation;
     onAction?: (id: string) => void;
     disabled?: boolean;
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,11 @@
-import { User } from "@prisma/client";
+import { User , Listing} from "@prisma/client";
+
+export type SafeListings = Omit<
+ Listing ,
+ "createdAt"
+> & {
+    createdAt:string;
+}
 
 export type SafeUser =Omit<
  User,
