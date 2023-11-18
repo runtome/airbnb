9568fdf1d2f2afa2f93447d3b0dfcd1129dea77b
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-18 15:40:20 +0700

    Debug for Next version 14

--- next.config.js
+++ next.config.js
@@ -2,10 +2,25 @@
 const nextConfig = {
 
     images:{
-        domains:[
-            "avatars.githubusercontent.com",
-            "lh3.googleusercontent.com",
-            "res.cloudinary.com"
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                pathname: '**',
+              },
+              {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '**',
+              },
+              {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '**',
+              },
+            // "avatars.githubusercontent.com",
+            // "lh3.googleusercontent.com",
+            // "res.cloudinary.com"
         ]
     }
 }
