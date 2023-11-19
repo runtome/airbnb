5e1aa1d691463da793b67316596c7b662a254d4f
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-19 11:30:22 +0700

    Individual Listing View: 5 ListingCategory

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -4,6 +4,8 @@ import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import { IconType } from "react-icons";
 import Avatar from "../Avatar";
+import ListingCategory from "./ListingCategory";
+import Map from "../Map";
 
 interface ListingInfoProps{
     user: SafeUser,
@@ -66,7 +68,21 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
                     </div>
                 </div>
             </div>
-            
+            <hr />
+            {category && (
+                <ListingCategory 
+                 icon={category.icon}
+                 label={category?.label}
+                 description={category?.description}
+                />
+            )}
+            <hr />
+            <div className="
+             text-lg font-light text-neutral-500">
+                {description}
+            </div>
+            <Map center={coordiates} />
+
         </div>
     )
 }
