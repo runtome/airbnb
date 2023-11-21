b1d3344bdc612a9a3b9c350c985dcd38a33805ea
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-21 21:28:01 +0700

    config domain

--- next.config.js
+++ next.config.js
@@ -2,26 +2,31 @@
 const nextConfig = {
 
     images:{
-        remotePatterns:[
-            {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com',
-                pathname: '**',
-              },
-              {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                pathname: '**',
-              },
-              {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '**',
-              },
-            // "avatars.githubusercontent.com",
-            // "lh3.googleusercontent.com",
-            // "res.cloudinary.com"
+        domains:[
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com",
+            "res.cloudinary.com"
         ]
+        // remotePatterns:[
+        //     {
+        //         protocol: 'https',
+        //         hostname: 'avatars.githubusercontent.com',
+        //         pathname: '**',
+        //       },
+        //       {
+        //         protocol: 'https',
+        //         hostname: 'lh3.googleusercontent.com',
+        //         pathname: '**',
+        //       },
+        //       {
+        //         protocol: 'https',
+        //         hostname: 'res.cloudinary.com',
+        //         pathname: '**',
+        //       },
+        //     // "avatars.githubusercontent.com",
+        //     // "lh3.googleusercontent.com",
+        //     // "res.cloudinary.com"
+        // ]
     }
 }
 
