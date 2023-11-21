26ffa90813316658b02eac691aae120b306dd11c
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-11-21 22:04:34 +0700

    use

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsParams } from "./actions/getListing";
 import ClientOnly from "./components/ClientOnly";
